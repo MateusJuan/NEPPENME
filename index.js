d4e5153f9d73f179630f327007f83fec8dac6c3e
MateusJuan <169060996+MateusJuan@users.noreply.github.com>
2024-09-11 21:44:18 -0300

    kk

--- index.js
+++ index.js
@@ -5,10 +5,14 @@ function pesquisar(){
     alert("Você clicou no ícone de pesquisar!");
 };
 function mobile() {
-    var htmlContent = "<div class='cell'><img src='cell1.PNG' style='height: 200px;'><h2>Xiaomi Redmi 12</h2><p>R$ 1.251,15</p> </div><div class='cell'><img src='cell2.PNG' style='height: 200px;'><h2>Moto G84</h2><p>R$ 1.349,99</p></div> <div class='cell'><img src='cell3.PNG' style='height: 200px;'><h2>Motorola Razr 40</h2><p>R$ 3.499,00</p></div>";
-    document.getElementById("mobile").innerHTML = htmlContent;
-}
+    var htmlContent1 = "<div class='cell'><img src='cell1.PNG' style='height: 200px;'><h2>Xiaomi Redmi 12</h2><p>R$ 1.251,15</p> </div>";
+    var htmlContent2 = "<div class='cell'><img src='cell2.PNG' style='height: 200px;'><h2>Moto G84</h2><p>R$ 1.349,99</p></div> ";
+    var htmlContent3 = "<div class='cell'><img src='cell3.PNG' style='height: 200px;'><h2>Motorola Razr 40</h2><p>R$ 3.499,00</p></div>";
+    document.getElementById("mobile").innerHTML = htmlContent1 + htmlContent2 + htmlContent3;
+};
 function TVAV() {
-    var htmlContent = "<div class='cell'><img src='tv1.png' style='height: 170px;'><h2>Smart TV QLED 65'' Sasung 4K</h2><p>R$ 7.299,90</p> </div><div class='cell'><img src='tv2.png' style='height: 200px;'><h2>Samsung The Freestyle 2nd Gen Projetor Smart</h2><p>R$4.529,00</p></div> <div class='cell'><img src='cell3.PNG' style='height: 200px;'><h2>Motorola Razr 40</h2><p>R$ 3.499,00</p></div>";
-    document.getElementById("mobile").innerHTML = htmlContent;
-}
+    var htmlContent1 = "<div class='cell'><img src='tv1.png' style='height: 170px;'><h2>Smart TV QLED 65'' Sasung 4K</h2><p>R$ 7.299,90</p></div>";
+    var htmlContent2 = "<div class='cell'><img src='tv2.png' style='height: 200px;'><h2>Samsung The Freestyle 2nd Gen Projetor Smart</h2><p>R$4.529,00</p></div>";
+    var htmlContent3 = "<div class='cell'><img src='cell3.PNG' style='height: 200px;'><h2>Motorola Razr 40</h2><p>R$ 3.499,00</p></div>"
+    document.getElementById("mobile").innerHTML = htmlContent1 + htmlContent2 + htmlContent3;
+};
