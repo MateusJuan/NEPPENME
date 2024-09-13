670d1ae9b2d01cb37daa34dc14189331679808c1
Mateus juan <169060996+MateusJuan@users.noreply.github.com>
2024-09-13 12:32:24 -0300

    atualizacao - banco de dados

--- kkk.sql
+++ kkk.sql
@@ -6,5 +6,21 @@ CREATE TABLE cliente(
     senha VARCHAR(16) NOT NULL
 );
 CREATE TABLE produto(
-    
+    codigo_produto INTEGER UNIQUE KEY auto increment,
+    tipo VARCHAR (250) PRIMARY KEY,
+    marca VARCHAR (250) NOT NULL,
+    modelo VARCHAR (250) NOT NULL,
+    cor VARCHAR (250) NOT NULL,
+    capacidade VARCHAR (250),
+    preco FLOAT NOT NULL
+); 
+
+CREATE TABLE carrinho(
+    tipo VARCHAR(250) NOT NULL,
+    preco FLOAT NOT NULL,
+    qtd INTEGER,
+    comprador VARCHAR(250) NOT NULL
+    FOREIGN KEY (produto) REFERENCES (tipo),
+    FOREIGN KEY (produto) REFERENCES (preco),
+    FOREIGN KEY (cliente) REFERENCES (nome)
 );
